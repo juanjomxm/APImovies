4d732036558b1634e7d24927bd69e571ff00932f
Juan Jose A. Zuluaga <juaninfante29@gmail.com>
2023-06-05 17:43:02 -0500

    inciando proyecto

--- movies.js
+++ movies.js
@@ -1,1 +1,24 @@
-const KEY_API = "53574b5a2a01901fcd5a04b11aced379"
+const KEY_API = '53574b5a2a01901fcd5a04b11aced379'
+
+async function getTrendingMoviesPreview(){
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + KEY_API)
+    const data = await res.json()
+
+    const movies = data.results
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+        const movieContainer = document.createElement('div')
+        movieContainer.classList.add('movie-container')
+
+        const movieImg = document.createElement('img')
+        movieContainer.classList.add('movie-img')
+        movieImg.setAttribute('alt', movie.title)
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie['backdrop_path'])
+
+        movieContainer.appendChild(movieImg)
+        trendingPreviewMoviesContainer.appendChild(movieContainer)
+        
+    });
+    console.log({data, movies})
+}
+getTrendingMoviesPreview()
